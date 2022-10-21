182a40254689e3c0abcbf8908fa1d1022fb0dd53
Yerkebulan Bekmukhayev <bekmukhayev@gmail.com>
2022-10-21 16:54:00 +0600

    String

---
+++ string.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string s = "Basketball";
+  cout << s.substr(0, 6) << endl;
+  
+  string str = "1234"; // 1234 = string
+  int n = stoi(str); // 1234 = integer 
+  cout << n + 5 << " " << str + '5';
+  
+  string st = "Lambda";
+  st.erase(2,4);
+  cout<< st << endl;
+  
+  //s = ""; == s.clear();
+  
+  //s += " Memorial";  == s.append(" Memoriallll");
+  
+  s.insert(3, "");
+  cout <<s;
+  
+  char a, b;
+  if(a == b){ cout << "Draw";}
+  if((a == 'R' && b == 'S') || (a == 'S' && b == 'P') || (a == 'P' && b == 'R')){ cout << a;}
+  if((b == 'R' && a == 'S') || (b == 'S' && a == 'P') || (b == 'P' && a == 'R')){ cout << b;}
+  }
+        i j
+    int c,d;
+    if(max < a[i][j]
+    max = a[i][j]
+    c = i;
+    d = j;
+    
+    cout << c << ' ' << d;
+
