d76edaee692423779f8cbd7134f41227f9edb199
Yerkebulan Bekmukhayev <bekmukhayev@gmail.com>
2022-10-26 17:20:34 +0600

    /

--- b.cpp
+++ b.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main(){
     string s;
-    getline(cin, s);
+    getline(s);
     for(int i = 0; i < s.size(); i++){
-        if(s[i] > 96){
+        if(s[i] >= 97 && s[i] <= 122) cnt1++; 
+        if(s[i] >= 65 && s[i] <= 90) cnt2++;
+        /*if(s[i] > 96){
             s[i] -= 32;
-        }
+        }*/
     }
     cout << s;
 }
